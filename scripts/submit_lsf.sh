6171edf92416c5c371a7e4010d884ac1ddf12bc3
Michael Hall <mbhall88@gmail.com>
2019-05-06 09:48:35 +1000

    update master process memory allocation

--- scripts/submit_lsf.sh
+++ scripts/submit_lsf.sh
@@ -2,9 +2,10 @@
 CLUSTER_CMD=("bsub -n {threads} -R \"select[mem>{resources.mem_mb}] rusage[mem={resources.mem_mb}] span[hosts=1]\" -M {resources.mem_mb} -o {cluster.output} -e {cluster.error} -J {cluster.name} -q {cluster.queue}")
 JOB_NAME=snakemake_master_process
 LOG_DIR=logs/
+MEMORY=1500
 
-bsub -R "select[mem>1000] rusage[mem=1000]" \
-    -M 1000 \
+bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY]" \
+    -M "$MEMORY" \
     -o "$LOG_DIR"/cluster_"$JOB_NAME".o \
     -e "$LOG_DIR"/cluster_"$JOB_NAME".e \
     -J "$JOB_NAME" \
